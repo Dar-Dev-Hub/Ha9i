f2fe618b99ef47e5d651760e4ebc07088663eae3
Khaledxab <khaledxab@gmail.com>
2024-06-13 20:33:53 +0100

    material3 + debug FALSE

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,10 @@ class Ha9i extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false, 
+      theme: ThemeData(
+        useMaterial3: true, 
+      ),
       home: WelcomePage(),
     );
   }
