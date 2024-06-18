eee900081c9cbdea087c902ab04048f98775ca4b
Khaledxab <khaledxab@gmail.com>
2024-06-18 16:03:48 +0100

    login controller

---
+++ lib/controllers/login_contoller.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  final formKey = GlobalKey<FormState>();
+  var obscureText = true.obs;
+
+  String? emailValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your email';
+    }
+    if (!RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$').hasMatch(value)) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
+
+  String? passwordValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your password';
+    }
+    return null;
+  }
+
+  void toggleObscureText() {
+    obscureText.value = !obscureText.value;
+  }
+
+  void login() {
+    if (formKey.currentState!.validate()) {
+      // Implement login logic here
+    }
+  }
+}
